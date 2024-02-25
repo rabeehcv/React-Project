e9f23c8d751052a46a62a7b9fde860988b857349
rabeehcv <rabeehcv28@gmail.com>
2024-02-25 08:13:17 +0530

    Update App.jsx

--- 1- Birthday-Reminder/src/App.jsx
+++ 1- Birthday-Reminder/src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import data from './data';
+import List from './List';
 
 function App (){
-  return <h2>Birthday Reminder</h2>;
-}
+  return (
+  <main>
+    <section className="container">
+      <h3>0 Birthdays Today</h3>
+      <List />
+    </section>
+  </main>
+  );
+};
 export default App;
