138c78fc1d69d6c316c1d1e898a8a95d0e5dc905
rabeehcv <rabeehcv28@gmail.com>
2024-03-03 19:49:18 +0530

    Upload Tours component

---
+++ 2- Tours/src/Tours.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tours = () => {
+    return (
+        <div className="tours">
+            <h2> Tours component</h2>
+        </div>
+    )
+}
+
+export default Tours;
