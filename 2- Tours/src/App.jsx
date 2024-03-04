3fab09eb3ea03aee679cd5a3a2c0b8fc6506b15b
rabeehcv <rabeehcv28@gmail.com>
2024-03-04 10:08:53 +0530

    Update with UseEffect

--- 2- Tours/src/App.jsx
+++ 2- Tours/src/App.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Loading from './Loading';
 import Tours from './Tours';
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  const [loading,setLoading] = useState(true)
+  const [loading,setLoading] = useState(false)
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   if(loading){
     return (
     <main>
