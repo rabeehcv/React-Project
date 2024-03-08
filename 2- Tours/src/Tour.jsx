0fd3e60d2f358ba1fd273688adc11fd16cea2f1d
rabeehcv <rabeehcv28@gmail.com>
2024-03-08 11:21:10 +0530

    Update Tour components

--- 2- Tours/src/Tour.jsx
+++ 2- Tours/src/Tour.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Tour = () => {
-    return <h2> Single  Tour component</h2>
+const Tour = ({ id, image, price, name, info }) => {
+    return (
+        <article className='single-tour'>
+            <img src={image} alt={name} />
+            <footer>
+                <div className='tour-info'>
+                    <h4>{name}</h4>
+                </div>
+            </footer>
+        </article>
+    )
 }
 
 export default Tour;
