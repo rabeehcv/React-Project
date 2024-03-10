46de6f3554b39d49bfced86fe19fb35c7fd0f10f
rabeehcv <rabeehcv28@gmail.com>
2024-03-10 12:44:28 +0530

    Update Not interest button

--- 2- Tours/src/Tour.jsx
+++ 2- Tours/src/Tour.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 
-const Tour = ({ id, image, price, name, info }) => {
-    const [readMore, setReadMore] = useState(false);
+const Tour = ({id,image,price,name,info,removeTour}) => {
+    const [readMore,setReadMore] = useState(false);
     return (
         <article className='single-tour'>
             <img src={image} alt={name} />
@@ -10,13 +10,13 @@ const Tour = ({ id, image, price, name, info }) => {
                     <h4>{name}</h4>
                     <h4 className='tour-price'>${price}</h4>
                 </div>
-                <p>{readMore ? info : `${info.substring(0, 200)}...`
-                }
-                    <button onClick={() => setReadMore(!readMore)}>
+                <p>{ readMore ? info : `${info.substring(0,200)}...`
+                    }
+                    <button onClick = {() => setReadMore(!readMore)}>
                         {readMore ? 'Show Less' : 'Read More'}
                     </button>
-                </p>
-                <button className='delete-btn'>Not Interested</button>
+                    </p>
+                <button className='delete-btn' onClick={() => removeTour(id)}>Not Interested</button>
             </footer>
         </article>
     )
