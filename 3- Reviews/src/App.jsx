32a3a86b8861fba9cad1d46f6869b77bcdd4de37
rabeehcv <rabeehcv28@gmail.com>
2024-03-12 11:46:32 +0530

    Update App.jsx

--- 3- Reviews/src/App.jsx
+++ 3- Reviews/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Review from './Review';
 
-function App(){
+function App() {
   return (
-    <h2> Review Project Setup</h2>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>Our Reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   )
 }
 
