35baafd41bd18a7c25e94dc0ae0282a51899dcba
rabeehcv <rabeehcv28@gmail.com>
2024-03-13 15:45:54 +0530

    Add button

--- 3- Reviews/src/Review.jsx
+++ 3- Reviews/src/Review.jsx
@@ -1,22 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import people from './data';
-import {FaQuoteRight} from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-    const [index,setIndex] = useState(0);
-    const {name,job,image,text}=people[index];
+    const [index, setIndex] = useState(0);
+    const { name, job, image, text } = people[index];
+
+    const checkNumber = (number) => {
+        if (number > people.length - 1) {
+            return 0;
+        }
+        if (number < 0) {
+            return people.length - 1;
+        }
+        return number;
+    }
+    const prevPerson = () => {
+        setIndex((index) => {
+            let newIndex = index - 1;
+            return checkNumber(newIndex);
+        });
+    };
+    const nextPerson = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkNumber(newIndex);
+        });
+    };
     return (
         <article className='review'>
             <div className='img-container'>
                 <img src={image} alt={name} className='person-img' />
-                <span className='quote-icon'><FaQuoteRight/></span>
+                <span className='quote-icon'><FaQuoteRight /></span>
             </div>
             <h4 className='author'>{name}</h4>
             <p className='job'>{job}</p>
             <p className='info'>{text}</p>
+            <div >
+                <button className='prev-btn' onClick={prevPerson}>
+                    <FaChevronLeft />
+                </button>
+                <button className='next-btn' onClick={nextPerson}>
+                    <FaChevronRight />
+                </button>
+            </div>
         </article>
     )
 }
 
-export
- default Review;
+export default Review;
