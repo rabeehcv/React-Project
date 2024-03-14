adbd3bfedf993e7a8c78a01e0a13adbb7f1417e7
rabeehcv <rabeehcv28@gmail.com>
2024-03-14 15:07:19 +0530

    New Project set up

---
+++ 4- Find-Your-Answers/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import data from './data';
+
+function App () {
+  return <h2>Project setup</h2>;
+}
+
+export default App;
---
+++ 4- Find-Your-Answers/src/data.jsx
@@ -0,0 +1,24 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Do I have to allow the use of cookies?',
+      info:'You are not legally required to allow the use of cookies on your website, but providing cookie consent is often necessary to comply with privacy regulations like the General Data Protection Regulation (GDPR). However, it is essential to inform users and obtain their consent if you do use cookies to track their browsing behavior or personalize their experience.',
+    },
+    {
+      id: 2,
+      title: 'How do I change my My Page password?',
+      info:'To change your My Page password, log in to your account, navigate to the settings or account management section, select the option to change password, and follow the prompts to enter your current password and create a new one.',
+        
+    },
+    {
+      id: 3,
+      title: 'What is BankID?',
+      info:'BankID is a digital identification solution used primarily in Scandinavian countries like Sweden and Norway. It allows individuals to authenticate their identity online for various services, including banking, government, and e-commerce, by using their electronic identification issued by their bank.',
+    },
+    {
+      id: 4,
+      title: 'Whose birth number can I use?',
+      info:'You can only use your own birth number for BankID authentication. Sharing or using someone else s birth number is not permitted and may be illegal.',
+    },
+  ]
+  export default questions
