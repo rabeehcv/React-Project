1fc8e51083eb8abff80166251cadf9fc626b68fd
rabeehcv <rabeehcv28@gmail.com>
2024-04-03 23:06:34 +0530

    Add button

--- 5- Menu/src/Categories.jsx
+++ 5- Menu/src/Categories.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Categories = () => {
-    return <h3> Category component</h3>
-}
+const Categories = ({filterItem}) => {
+    return (
+        <div className='btn-container'>
+            <button className='btn' onClick={()=>filterItem('breakfast')}>Breakfast</button>
+            <button className='btn' onClick={()=>filterItem('lunch')}>Lunch</button>
+            <button className='btn' onClick={()=>filterItem('shakes')}>Shakes</button>
+        </div>
+    );
+};
 
 export default Categories;
