faccf5b6277f5cdade545d4019ff4c7751b80bad
rabeehcv <rabeehcv28@gmail.com>
2024-06-12 12:50:42 +0530

    Update App.jsx

--- 5- Menu/src/App.jsx
+++ 5- Menu/src/App.jsx
@@ -3,8 +3,12 @@ import items from './data';
 import SingleMenu from './Menu';
 import Category from './Categories';
 
+const allCategories =['all',...new Set(items.map((item) => item.category))];
+
+
 function App() {
   const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItem = (category) => {
     if(category==='all'){
@@ -20,7 +24,7 @@ function App() {
       <h2 className="menu-title">Our Menu</h2>
       <div className='underline'></div>
       <section className="menu-section">
-        <Category filterItem={filterItem}/>
+        <Category categories={categories} filterItem={filterItem}/>
         {menuItem.map((menuitem)=>{
           return <SingleMenu key={menuitem.id} {...menuitem}/>
         })}
