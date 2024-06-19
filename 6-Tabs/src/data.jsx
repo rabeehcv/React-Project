a92aff8dde048d672e8c8b9ca96245b0fe89e6e3
rabeehcv <rabeehcv28@gmail.com>
2024-06-19 12:48:02 +0530

    Upload data.jsx

---
+++ 6-Tabs/src/data.jsx
@@ -0,0 +1,25 @@
+const data = [
+    {
+      "id": "recAGJfiU4CeaV0HL",
+      "order": 3,
+      "title": "Full Stack Web Developer",
+      "dates": "December 2015 - Present",
+      "company": "IBM"
+    },
+    {
+      "id": "recIL6mJNfWObonls",
+      "order": 2,
+      "title": "Front-End Engineer",
+      "dates": "May 2015 - December 2015",
+      "company": "BIGDROP"
+    },
+    {
+      "id": "rec61x18GVY99hQq5",
+      "order": 1,
+      "title": "Engineering Intern",
+      "dates": "May 2014 - September 2015",
+      "company": "COOPER"
+    },
+  ];
+
+export default data;
