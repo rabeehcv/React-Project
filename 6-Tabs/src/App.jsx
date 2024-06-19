073cbf7fb3e00cdb98e8ca43ef40bc1aec35649e
rabeehcv <rabeehcv28@gmail.com>
2024-06-19 12:47:46 +0530

    Update App.jsx

--- 6-Tabs/src/App.jsx
+++ 6-Tabs/src/App.jsx
@@ -1,18 +1,28 @@
-import React,{useState} from 'react';
-const url = 'https://course-api.com/react-tabs-project'
-function App(){
-  const [loading,setLoading] = useState(true);
-  const [value,setValue] = useState(0);
-  const [jobs,setJobs] = useState([]);
+import React, { useState, useEffect } from 'react';
+import items from './data';
+import SingleJob from './job';
 
-  const fetchJobs = async () => {
-    const response = await fetch(url);
-    const newjobs = await response.json();
-    setJobs(newjobs)
-    setLoading(false)
-  };
 
-  return <h2>Tabs SetUp</h2>;
+
+function App() {
+  const [jobs, setJobs] = useState(items);
+  const [value, setValue] = useState(0);
+
+  return (
+    <main>
+      <div className='title'>
+        <h2> Experience</h2>
+        <div className='underline'></div>
+      </div>
+      <section className='job-section'>
+        {jobs.map((job) => {
+          return <SingleJob key={job.id} {...job} />
+        })
+        }
+      </section>
+    </main>
+  )
+
 }
 
 export default App;
