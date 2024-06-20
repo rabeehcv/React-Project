63d22266bb9afbc37c4007f81bab649e1c3bd8e2
rabeehcv <rabeehcv28@gmail.com>
2024-06-20 15:03:38 +0530

    Update App.jsx

--- 6-Tabs/src/App.jsx
+++ 6-Tabs/src/App.jsx
@@ -1,28 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React,{useState} from 'react';
 import items from './data';
 import SingleJob from './job';
+import Category from './categories';
+
+const companies=['ALL',...new Set(items.map((item)=>item.company))];
 
 
+function App(){
+  const [jobs,setJobs] = useState(items);
 
-function App() {
-  const [jobs, setJobs] = useState(items);
-  const [value, setValue] = useState(0);
-
+  const filterItem = (company) => {
+    if(company==='ALL'){
+      return setJobs(items)
+    };
+    const newItems = items.filter((item)=>
+      item.company===company
+    )
+    setJobs(newItems)
+    };  
+  
   return (
-    <main>
-      <div className='title'>
-        <h2> Experience</h2>
-        <div className='underline'></div>
-      </div>
-      <section className='job-section'>
-        {jobs.map((job) => {
-          return <SingleJob key={job.id} {...job} />
-        })
-        }
-      </section>
-    </main>
-  )
-
+  <main>
+    <div className='title'>
+      <h2> Experience</h2>
+      <div className='underline'></div>
+    </div>
+    <section className='job-section'>
+      <Category companies={companies} filterItem ={filterItem}  />
+      {jobs.map((job)=>{
+       return <SingleJob key={job.id} {...job}/>
+      })}
+    </section>
+  </main>
+  );
 }
 
 export default App;
