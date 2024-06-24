66961224c00e074c00591cfa8ecd3bf6a456a037
rabeehcv <rabeehcv28@gmail.com>
2024-06-24 12:45:20 +0530

    Final setup

--- 1- Birthday-Reminder/vite.config.js
+++ 1- Birthday-Reminder/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/React-Project/1-%20Birthday-Reminder/',
+  plugins: [react()]
 })
