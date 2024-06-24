dd483cc8f503f01b2936d1420596907176a85cb4
rabeehcv <rabeehcv28@gmail.com>
2024-06-24 13:15:02 +0530

    Rename folder name

---
+++ 1-Birthday-Reminder/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/React-Project/1-%20Birthday-Reminder/',
+  plugins: [react()]
+})
